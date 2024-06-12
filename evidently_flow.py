fd574ffa36e964ba17bf25f9a5d5720741575e40
RedPandaMC <jurreandenys@gmail.com>
2024-06-12 12:56:45 +0000

    yes

--- evidently_flow.py
+++ evidently_flow.py
@@ -9,6 +9,7 @@ from evidently.metrics import ColumnDriftMetric
 from evidently.metrics import DatasetDriftMetric
 from evidently.metrics import DatasetMissingValuesMetric
 from evidently import ColumnMapping
+from prefect import flow, task
 
 import psycopg
 
@@ -33,9 +34,9 @@ COL_MAPPING = ColumnMapping(
     target=None
 )
 
-# host, port, user, password
 CONNECT_STRING = f'host={os.getenv("POSTGRES_HOST")} port={os.getenv("POSTGRES_PORT")} user={os.getenv("POSTGRES_USER")} password={os.getenv("POSTGRES_PASSWORD")}'
 
+@task(name='Prepare Database')
 def prep_db():
     create_table_query = """
     DROP TABLE IF EXISTS metrics;
@@ -55,6 +56,7 @@ def prep_db():
         with psycopg.connect(f'{CONNECT_STRING} dbname=test') as conn:
             conn.execute(create_table_query)
 
+@task(name='Load Data & Model')
 def prep_data():
     ref_data = pd.read_parquet('data/reference.parquet')
     with open('models/lin_reg.bin', 'rb') as f_in:
@@ -64,9 +66,20 @@ def prep_data():
 
     return ref_data, model, raw_data
 
+@flow(name='Calculate Metrics')
 def calculate_metrics(current_data, model, ref_data):
     current_data['prediction'] = model.predict(current_data[NUMERICAL + CATEGORICAL].fillna(0))
 
+    report = create_report(current_data, ref_data)
+
+    prediction_drift = report['metrics'][0]['result']['drift_score']
+    num_drifted_cols = report['metrics'][1]['result']['number_of_drifted_columns']
+    share_missing_vals = report['metrics'][2]['result']['current']['share_of_missing_values']
+
+    return prediction_drift, num_drifted_cols, share_missing_vals
+
+@task(name='Create Report')
+def create_report(current_data, ref_data):
     report = Report(metrics = [
         ColumnDriftMetric(column_name='prediction'),
         DatasetDriftMetric(),
@@ -80,13 +93,9 @@ def calculate_metrics(current_data, model, ref_data):
     )
 
     result = report.as_dict()
+    return result
 
-    prediction_drift = result['metrics'][0]['result']['drift_score']
-    num_drifted_cols = result['metrics'][1]['result']['number_of_drifted_columns']
-    share_missing_vals = result['metrics'][2]['result']['current']['share_of_missing_values']
-
-    return prediction_drift, num_drifted_cols, share_missing_vals
-    
+@task(name='Save Metrics To DB')
 def save_metrics_to_db(cursor, date, prediction_drift, num_drifted_cols, share_missing_vals):
     cursor.execute("""
     INSERT INTO metrics(
@@ -98,7 +107,7 @@ def save_metrics_to_db(cursor, date, prediction_drift, num_drifted_cols, share_m
     VALUES (%s, %s, %s, %s);
     """, (date, prediction_drift, num_drifted_cols, share_missing_vals))
     
-
+@flow(name='Monitor Flow')
 def monitor():
     startDate = datetime.datetime(2022, 2, 1, 0, 0)
     endDate = datetime.datetime(2022, 2, 2, 0, 0)
@@ -115,14 +124,18 @@ def monitor():
                 current_data = raw_data[(raw_data.lpep_pickup_datetime >= startDate) &
                                         (raw_data.lpep_pickup_datetime < endDate)]
                 
-                prediction_drift, num_drifted_cols, share_missing_vals = calculate_metrics(current_data, model, ref_data)
-                save_metrics_to_db(cursor, startDate, prediction_drift, num_drifted_cols, share_missing_vals)
+                create_metrics(startDate, ref_data, model, cursor, current_data)
 
                 startDate += datetime.timedelta(1)
                 endDate += datetime.timedelta(1)
 
                 time.sleep(1)
                 print("data added")
+
+@flow()
+def create_metrics(startDate, ref_data, model, cursor, current_data):
+    prediction_drift, num_drifted_cols, share_missing_vals = calculate_metrics(current_data, model, ref_data)
+    save_metrics_to_db(cursor, startDate, prediction_drift, num_drifted_cols, share_missing_vals)
 
 if __name__ == '__main__':
     monitor()
